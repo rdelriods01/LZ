17fbb876d2833b9a0d49c2a08ba624276ca086ed
Ricardo <rdelrio.ds@gmail.com>
2018-02-22 04:00:11 -0600

    Menus OK

--- src/app/components/addPlatillos.component.ts
+++ src/app/components/addPlatillos.component.ts
@@ -19,23 +19,17 @@ export class AddPlatillosComponent implements OnInit{
     descripcion:String;
     temp:any;
     sugerencias:any;
-    newM:Boolean;
     seleccionados:any[];
 
     constructor(public dialogRef: MdDialogRef<AddPlatillosComponent>,public platilloService: PlatilloService){
-   
     }
 
     ngOnInit(){
         this.sugerencias=this.platilloService.getPlatillos(this.tiempo);
-        if(this.newM==true){
-            this.seleccionados=[];
-        } 
     }
 
     select(p){
         let n=this.seleccionados.length;
-        console.log(n);
         if(n==0){
             this.seleccionados.push(p);
             for(let i=0;i<this.sugerencias.length;i++){
@@ -43,7 +37,6 @@ export class AddPlatillosComponent implements OnInit{
                     this.sugerencias[i].flag=true;
                 }
             }
-            console.log(this.seleccionados);
         }else{
             let yasta:Boolean=false;
             for(let i=0;i<n;i++){
@@ -51,7 +44,6 @@ export class AddPlatillosComponent implements OnInit{
                     yasta=true;
                     this.seleccionados.splice(i,1);
                     n--;
-                    console.log(this.seleccionados);
                     for(let j=0;j<this.sugerencias.length;j++){
                         if(p.platillo==this.sugerencias[j].platillo){
                             this.sugerencias[j].flag=false;
@@ -66,24 +58,35 @@ export class AddPlatillosComponent implements OnInit{
                         this.sugerencias[j].flag=true;
                     }
                 }
-                console.log(this.seleccionados);
             }
         }      
     }
 
     addPlatillo(){
-        this.temp={id:"",platillo:this.platillo,descripcion:this.descripcion,f:true};
-        // this.platilloService.savePlatillo(this.temp,t);
+        this.temp={id:"",platillo:this.platillo,descripcion:this.descripcion,flag:true};
         this.seleccionados.push(this.temp);
         this.sugerencias.push(this.temp);
-
         this.platillo="";
         this.descripcion="";
         this.temp={};
     }
 
-    enviar(){
+    delPlatillo(p){
+        let n=this.seleccionados.length;
+        for(let i=0;i<n;i++){
+            if(p.platillo==this.seleccionados[i].platillo){
+                this.seleccionados.splice(i,1);
+                n--;
+                for(let j=0;j<this.sugerencias.length;j++){
+                    if(p.platillo==this.sugerencias[j].platillo){
+                        this.sugerencias[j].flag=false;
+                    }
+                }
+            }
+        }
+    }
 
+    enviar(){
         this.dialogRef.close(this.seleccionados);
     }
 
--- src/app/components/menus.component.ts
+++ src/app/components/menus.component.ts
@@ -1,5 +1,5 @@
-import { Component, Input, Output, EventEmitter, OnChanges, OnInit} from '@angular/core';
-import {MdDialog, MdDialogRef, MdTabChangeEvent} from '@angular/material';
+import { Component, Input, Output, EventEmitter, OnChanges} from '@angular/core';
+import {MdDialog, MdDialogRef} from '@angular/material';
 
 import { AddPlatillosComponent } from './addPlatillos.component';
 
@@ -11,83 +11,44 @@ import {VisitaService} from '../services/visita.service';
   styleUrls: ['../css/menus.css'],
   providers:[VisitaService]
 })
-export class MenusComponent implements OnChanges, OnInit {
+export class MenusComponent implements OnChanges{
 
-    @Input() paciente:any;
-    @Input() Citas:any;
-    @Input() yaConsulto:Boolean;
-
+    @Input() Cita:any;
     @Output() yaGuardado = new EventEmitter<string>();
 
     temp:any;
-    save:Boolean=false;
-    inicial:any;
-    nTabs:any;
-    nuevoMenu:Boolean=false;
+    mes:string[]=[
+        'ene','feb','mar','abr','may','jun','jul','ago','sep','oct','nov','dic'
+    ];
+    fechaPrint:any;
 
     constructor(public dialog: MdDialog,public visitaService: VisitaService,){
         this.clearTemp();
-        // console.log(this.Citas);
-        // this.nTabs=this.Citas.length-1;
     }
-    ngOnInit(){
-        // console.log(this.Citas);
-        // this.nTabs=this.Citas.length-1;
-    }
-    indexviejo:any[]=[];
 
     ngOnChanges(){
-        console.log(this.Citas);
-        this.nTabs=this.Citas.length-1;
-        this.indexviejo.push(this.nTabs);
-        // let n=this.nTabs.toString();
-        if(!this.yaConsulto){ this.Citas.shift() }
-        if(this.Citas[this.nTabs].menuF==false){
-            this.save=true;
+        console.log(this.Cita);
+        if(this.Cita.menuCompleto){
+            this.temp=this.Cita.menuCompleto;
         }
+        let temp=this.Cita.fecha.split("-");
+        for(let j=1;j<=12;j++){
+            if(temp[1]==j){
+                temp[1]=this.mes[j-1];
+            }
+        }
+        this.fechaPrint=(temp[2] + " de " + temp[1] + " de " + temp[0]);
     }
 
     clearTemp(){
         this.temp={desayuno:[],comida:[],cena:[],snack:[]}
     }
 
-    onTabChange(e: MdTabChangeEvent){
-        console.log('index => ', e.index);
-        this.indexviejo.push(e.index);
-        let n = this.indexviejo[this.indexviejo.length-2];
-        console.log(n);
-        if(e.index==this.nTabs){
-            this.save=true;
-            this.Citas[e.index].menuF=false;
-            this.clearTemp();
-            if(this.save==false){  this.Citas[n].menuCompleto=this.inicial }
-        }else{
-            if(this.save==false){
-                this.Citas[e.index].menuF=true;
-                this.Citas[n].menuCompleto=this.inicial;
-                this.save=true;
-
-            //     this.clearTemp();
-            }
-        }
-        console.log(this.Citas);
-    }
-    
-    openPlatillosDialog(C,T){ //lee a cual cita corresponde, y el tiempo de comida
-        if(C.menuF==false){ //si es false(esta unlock o es nuevo)
+    openPlatillosDialog(T){ //lee a cual cita corresponde, y el tiempo de comida
+        if(this.Cita.menuF==false){ //si es false(esta unlock o es nuevo)
             let dialogRef = this.dialog.open(AddPlatillosComponent);
             dialogRef.componentInstance.tiempo=T; //enviar al dialog el tiempo
-            if(!C.menuCompleto){ this.nuevoMenu=true}else{this.nuevoMenu=false}
-            if(this.nuevoMenu==true){
-                dialogRef.componentInstance.newM=true; //avisar que es nuevo menu
-            }else {
-                dialogRef.componentInstance.newM=false;
-                console.log('no es nuevo, está unlock');
-                console.log(C.menuCompleto[T]);
-                dialogRef.componentInstance.seleccionados=this.temp[T];
-            } 
-
-            // if(this.save==false){dialogRef.componentInstance.seleccionados=C.menuCompleto[T]}
+            dialogRef.componentInstance.seleccionados=this.temp[T];
             dialogRef.afterClosed().subscribe(result => { //recoger del dialog
                 if(result!=null){
                     this.temp[T]=result;
@@ -96,43 +57,42 @@ export class MenusComponent implements OnChanges, OnInit {
         }
     }
 
-    unlock(C){
-        this.inicial = JSON.parse(JSON.stringify(C.menuCompleto))
-        this.temp = JSON.parse(JSON.stringify(C.menuCompleto))
-        this.save=false;
-        C.menuF=false;
-    }
-
-    lock(C){
-        C.menuCompleto=this.inicial;
-        this.save=true;
-        C.menuF=true;
-    }
-
-    guardar(C){
+    guardar(){
         if(this.temp.desayuno.length==0 || this.temp.comida.lengt==0 || this.temp.cena.length==0 || this.temp.snack.length==0 ){
             alert('Es necesario llenar todos los tiempos de comida para poder guardar el menú');
         }else{
-            C.menuF=true;
-            C.menuCompleto=this.temp;
-            // volver a poner la fecha como es
-            let dateSplit =  C.fecha.split("-");
-            let mes:string[]=[
-                'ene','feb','mar','abr','may','jun','jul','ago','sep','oct','nov','dic'
-            ];
-            for(let j=1;j<=12;j++){
-                if(dateSplit[1]==mes[j-1]){
-                    dateSplit[1]=j;
-                    if(dateSplit[1]<10){
-                        dateSplit[1]=('0'+dateSplit[1]);
-                    }
-                }
-            }
-            C.fecha=(dateSplit[0]+'-'+dateSplit[1]+'-'+dateSplit[2]);
-            this.visitaService.editVisita(C.id,C);
+            this.Cita.menuF=true;
+            this.Cita.menuCompleto=this.temp;
+            console.log(this.Cita.fecha);
+            this.formatoFecha();
+            this.visitaService.editVisita(this.Cita.id,this.Cita);
             this.yaGuardado.next();
-            this.save=true;
             this.clearTemp();
         }
+    }
+
+    imprimir(){
+        window.print();
+    }
+    
+    editarMenu(){
+        this.Cita.menuF=false;
+        this.formatoFecha();
+        this.visitaService.editVisita(this.Cita.id,this.Cita);
+        this.yaGuardado.next();
+    }
+
+
+    formatoFecha(){
+        let dateSplit =  this.Cita.fecha.split("-");
+        for(let j=1;j<=12;j++){
+            if(dateSplit[1]==this.mes[j-1]){
+                dateSplit[1]=j;
+                if(dateSplit[1]<10){
+                    dateSplit[1]=('0'+dateSplit[1]);
+                }
+            }
+        }
+        this.Cita.fecha=(dateSplit[0]+'-'+dateSplit[1]+'-'+dateSplit[2]);
     }
 }
