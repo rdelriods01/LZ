41f5468458fb34c35053d2d4f7c2436cdbbc7518
Ricardo Del Rio <rdelrio.ds@gmail.com>
2020-01-26 23:55:50 -0600

    add firebase production

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyA78Lla-odvclSMvdIZnWE_lrwK4556DcQ",
+    authDomain: "lightzone-c9d48.firebaseapp.com",
+    databaseURL: "https://lightzone-c9d48.firebaseio.com",
+    projectId: "lightzone-c9d48",
+    storageBucket: "lightzone-c9d48.appspot.com",
+    messagingSenderId: "326970238930"
+  }
 };
